3d867ab271bdd30045ed14b9a4ebcd0a17734270
Sam-wing <346431045@qq.com>
2021-05-12 19:21:09 +0800

    第三次提交

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,11 @@
+import controller.UserController;
+import controller.impl.UserControllerImpl;
+
+public class UserTest {
+    public static void main(String[] args) {
+        UserController controller = new UserControllerImpl();
+        controller.register(1,"123","123456");
+        controller.login("123","123456");
+    }
+
+}
